fdb62fda48c845fb2c108bae9fc9a3842aee6718
mauricio pineda <map1900@outlook.com>
2021-08-06 16:47:12 -0400

    Yelp MVP - models finished and passed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Limpiando base de datos...'
+Restaurant.destroy_all
+puts 'La base de datos ha sido limpiada'
+
+puts 'Creating records for database, hold on...'
+100.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w(chinese italian japanese french belgian).sample
+    # category: CATEGORIES.sample # global variable perhaps?
+  )
+  puts "Restaurant #{restaurant.id} is created"
+end
+ 
+puts 'Hecho'
+
